ef764acfaa1dc591f14f611325f9f4f8109a52ad
AlexHuntsman <ahun0442@740s-pl-118.local>
2015-12-17 09:15:12 -0700

    Created color change app with preset colors

--- ColorChangeSwift/ViewController.swift
+++ ColorChangeSwift/ViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBAction func randomColor(sender: UIButton)
+    {
+        
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
+    @IBAction func redColor(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.redColor()
+    }
+    
+    @IBAction func blueColor(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.blueColor()
+    }
+    
+    @IBAction func greenColor(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.greenColor()
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
